48023df9ca1c1d46bbb33e297d50ef45c6bdd111
Taufiq Pohan <m.taufiq30s@hotmail.com>
2020-12-13 02:11:50 +0700

    Fix wrong var name 'i'

--- src/detector.c
+++ src/detector.c
@@ -380,7 +380,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
         draw_train_loss(windows_name, img, img_size, avg_loss, max_img_loss, iteration, net.max_batches, mean_average_precision, draw_precision, "mAP%", avg_contrastive_acc / 100, dont_show, mjpeg_port, avg_time);
 #endif    // OPENCV
 
-        if (i % 1000 == 0 || (i < 1000 && i % 100 == 0)) {
+        if (iteration % 1000 == 0 || (iteration < 1000 && iteration % 100 == 0)) {
         //if (i % 100 == 0) {
 //         if (iteration >= (iter_save + 10000) || iteration % 10000 == 0) {
 //             iter_save = iteration;
@@ -388,7 +388,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
             if (ngpus != 1) sync_nets(nets, ngpus, 0);
 #endif
             char buff[256];
-            sprintf(buff, "%s/%s_%d.weights", backup_directory, base, i);
+            sprintf(buff, "%s/%s_%d.weights", backup_directory, base, iteration);
             save_weights(net, buff);
         }
 
